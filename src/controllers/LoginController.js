dcb73b12ab0d8a064184a63e8fd2e36b47e0c37c
Mário Cesar <mc.fialho@hotmail.com>
2022-10-14 20:41:03 -0300

    incrementando metodo post

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -4,12 +4,23 @@ class LoginController extends HttpController {
   //sobescreve o método da classe base
   routesConfig(baseUrl) {
     //define a rota e o manipulador da classe login
-    this.express.post(`${baseUrl}/login`, (req, res) => {
-      this.login(req, res);
-    });
+    //esse metodo bind permite pegar a referência direto do metodo login, então não precisa passar uma callback com req e res como argumentos.
+    //passando o método login como referência e informando que o contexto que deve ser usado é da próprio objeto da classe LoginController.
+    this.express.post(`${baseUrl}/login`, this.login.bind(this));
   }
 
   login(req, res) {
+    //atribui o corpo da solicitação para a variável body
+    const body = req.body;
+
+    //valida se foi passado no body os campos de login e senha
+    if(!body || !body.login || !body.password) {
+      //retorna um erro para quem chamou a api falando que os parametros estão inválidos
+      return res.status(400).json({
+        status: 400,
+        erro: "Invalid enter parameters"
+      });
+    }
     //devolve a resposta mockada do login em formato json
     res.json({
       token: 'token gerado pela APi'
