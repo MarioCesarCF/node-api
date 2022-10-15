80498b269823cc949336a1b31af5f5756a35da70
Mário Cesar <mc.fialho@hotmail.com>
2022-10-14 21:21:05 -0300

    swagger

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger/swagger.json');
 const LoginController = require('./controllers/LoginController');
+const AppConstants = require('./enum/AppConstants');
 
 class App {
   #controllers;
@@ -21,6 +24,9 @@ class App {
     //registra os middlewares para fazer a conversão das requisições da API
     this.express.use(express.urlencoded({ extended: true }));
     this.express.use(express.json());
+
+    //configura o swagger da app para servir a documentação
+    this.express.use(`${AppConstants.BASE_API_URL}/docs`, swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
     //registra o middleware para fazer log das requisições
     this.express.use((req, res, next) => {
